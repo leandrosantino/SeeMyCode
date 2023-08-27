a62a8df7355f12a0de236300d8571bd039f5acd3
leandrosantino <leandrosantino2013@gmail.com>
2023-08-26 21:20:11 -0300

    fix bug

--- src/components/Teste.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useMenu } from "@/hooks/useMenu"
-import { Explorer } from "./Explorer"
-
-export function NavBar() {
-  const menu = useMenu()
-  return <Explorer show={menu.show} />
-}
